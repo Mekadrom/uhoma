77ed69987fb257fdb534e5bedc80945e1f4b9243
Mekadrom <zadarimm@gmail.com>
2021-06-19 16:58:34 -0400

    added login ui screen

--- node-ui/src/app/home/home.component.ts
+++ node-ui/src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  username: string = '';
+  password: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  omitSpecialChar(event: any) {
+    let k = event.charCode;
+    return((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57));
+  }
 }
